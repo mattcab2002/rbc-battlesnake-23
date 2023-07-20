15eb67421156e0d0d20de734eb7a435eeed455a5
Matthew Cabral <matthew.larbac@gmail.com>
2023-07-20 10:18:09 -0400

    Added logic to avoid ourselves
    Should be tested after the boundary checks are implemented.

--- main.py
+++ main.py
@@ -67,7 +67,29 @@ def move(game_state: typing.Dict) -> typing.Dict:
     # board_height = game_state['board']['height']
 
     # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-    # my_body = game_state['you']['body']
+    my_body = game_state['you']['body']
+    head = my_body[0]
+
+    temp = head
+    temp["x"] += 1
+    if temp in my_body[1:]:
+        is_move_safe["right"] = False
+
+    temp = head
+    temp["x"] -= 1
+    if temp in my_body[1:]:
+        is_move_safe["left"] = False
+
+    temp = head
+    temp["y"] += 1
+    if temp in my_body[1:]:
+        is_move_safe["up"] = False
+
+    temp = head
+    temp["y"] -= 1
+    if temp in my_body[1:]:
+        is_move_safe["down"] = False
+        
 
     # TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
     # opponents = game_state['board']['snakes']
